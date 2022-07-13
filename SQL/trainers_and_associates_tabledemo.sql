1babea06a82d3d1e98de8f12b24466b03663031e
jgomie123 <jackiemg21@gmail.com>
2022-07-13 08:21:42 -0700

    aggregate func, subqueries, referential integ., committing

--- SQL/trainers_and_associates_tabledemo.sql
+++ SQL/trainers_and_associates_tabledemo.sql
@@ -9,7 +9,9 @@ CREATE TABLE trainers();
 -- How to change a table after it has been created
 ALTER TABLE trainers ADD COLUMN trainer_name varchar(100);
 
- DROP TABLE trainers;
+DROP TABLE trainers;
+DROP TABLE associates; 
+DROP TABLE batches; 
 --Be careful when you drop tables - the data cannot be retreived
 
 --Constraints: 
@@ -115,7 +117,8 @@ INSERT INTO associates (associate_name, associate_locale, associate_hobby, train
 ('Myesha', 'Virginia', 'swimming', 1),
 ('Stephanie', 'Colorado', 'hiking', 4),
 ('Jonathan', 'California', NULL, 2),
-('Muhammad', 'Florida', 'Kayaking', 6)
+('Muhammad', 'Florida', 'Kayaking', 2)
+
 
 -- use ALTER to change columns after they have been made
 ALTER TABLE associates ALTER COLUMN associate_locale TYPE TEXT;
@@ -142,9 +145,9 @@ CREATE TABLE batches (
 );
 
 INSERT INTO batches (trainer_id, associate_id) VALUES 
-	(1, 1),
-	(1, 2),
-	(4, 3)
+	(1, 5),
+	(1, 6),
+	(4, 7)
 	
 SELECT * FROM batches;
  -- one to many: the same trainer can have multiple associates
@@ -166,4 +169,104 @@ SELECT * FROM trainers WHERE caffine_lvl > (SELECT avg (caffine_lvl) FROM traine
 -- this query is selecting all of the trainers that have a caffine level that is greater than the average
 -- the query finds out what is the average caffine level, then finds the trainers that have a higher than 
 -- avg lvl
+
+
+-- Aggregate functions
+-- these are functions that perform a calulation ona  set of rows and they return a single ROW
+-- Some aggregate functions: 
+	-- avg() - get the average
+	-- count() - counting the total; for ex, how many trainers are in my trainer table
+	-- max() - the maximum value
+	-- min() - the minimum value
+	-- sum() - adding up the total
+
+-- COUNT function
+SELECT COUNT(DISTINCT(associate_name)) FROM associates;
+-- distinct keyword looks for unique records, does not count duplicates
+
+-- SUM function
+SELECT sum(caffine_lvl) FROM trainers;
+-- simple addition
+
+-- MIN funciton
+SELECT min(caffine_lvl) FROM trainers;
+
+
+-- Subqueries
+-- A subquery is a query within a query - subqueries are also know as inner queries 
+-- the "outer" query would also be known as the main query
+-- Subqueries are always enclosed within ()
+-- You can combine subqueries within select, insert, update, and delete commands, as well as
+-- WITH different operators (<, > =, <=, ...), as well as with aggregate functions as above
+
+SELECT * FROM trainers; 
+SELECT * FROM associates; 
+SELECT * FROM batches; 
+
+INSERT INTO trainers (trainer_name, campus, likes_coffee, caffine_lvl) VALUES 
+('Charles', 'Virginia', 't', 55),
+('Stephen', 'Virginia', 'f', 200)
+
+SELECT trainer_name, campus FROM trainers WHERE caffine_lvl = (SELECT min(caffine_lvl) FROM trainers);
+-- The below syntax is not allowed in Postgres; aggregate functions are not allowed in the WHERE position
+-- SELECT trainer_name FROM trainers WHERE caffine_lvl = (min(caffine_lvl));
+
+-- you can have subqueries that return more than one row only if they are used with multiple value operators
+-- multiple value operators include: IN, EXISTS, NOT IN, ANY/SOME, ALL 
+
+UPDATE trainers SET caffine_lvl = caffine_lvl + 10 WHERE caffine_lvl IN (SELECT caffine_lvl FROM trainers 
+WHERE caffine_lvl < 150);
+
+DELETE FROM trainers WHERE caffine_lvl IN (SELECT caffine_lvl FROM trainers WHERE caffine_lvl < 100) RETURNING trainer_name;
+
+
+-- Referential integrity
+-- Just to revisit this one more time: referential integrity refers to the relationships between tables;
+-- specifically the accuracy and consistency of data within that relationship
+-- This is referring to the use of foreign keys - the connection (relationship) between DATA
+-- in other tables; whenever a foreign key value is used, it must reference a valid existing primary key
+-- in the parent table
+
+
+-- TCL: Transaction Control Language
+-- THis sublanguage is concerned with groups sql commands and executing them as a batch
+-- Commands: Commit, Savepoint, Rollback, Transact
+-- For today we will only be focusing on COMMIT 
+
+-- Commit is used to save changes to your tables and reflect those changes in the database; the commit command
+-- is very essential in SQL, and is typically used after DML commands (or transactions - transactions 
+-- as simply a set of sql statements). When you execute a DML command without a commit statement, 
+-- the changes to the tables are only visible to you - they haven't actually been committed to the database itself
+-- Once you commit the changes, those changes become visible on the database itself
+
+-- if I want to commit my changes to the database, I must surround those statements with a begin block 
+BEGIN TRANSACTION; 
+INSERT INTO batches (trainer_id, associate_id) VALUES
+(1, 8)
+COMMIT; 
+
+-- one thing to keep in mind with these transactions - if one of my statements retuls in an error 
+-- while i am inside of the begin block, the subsequent commands after the error occured won't execute
+-- until the transaction is closed
+
+BEGIN TRANSACTION; 
+
+INSERT INTO associates (associate_name, associate_locale, associate_hobby, trainer_id) VALUES 
+('Ben', 'Florida', 'reading', 1)
+INSERT INTO batches (trainer_id, associate_id) VALUES
+(1, 9)
+ROLLBACK; -- you can ROLLBACK IF an error occurs inside OF the TRANSACTION block
+-- note that you cannot continue executing commands if there is a failure inside the block
+
+COMMIT; 
+-- if you commit your changes, they are guaranteed to be there 
+-- this is important in case there is a failure of some kind; without committing, your data is not
+-- guaranteed to still be there once the database is back up and running
+-- Some vendors autocommit to the database for you so you don't need to call the commit command
+-- but you can turn this autocommit on and off
+
+
+
+
+
 
